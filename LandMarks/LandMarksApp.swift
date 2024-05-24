e9af8c3dd9d8748e04285e8a764f60feaebf1190
AshrafSU <mannabteb@gmail.com>
2024-05-24 13:09:17 +0600

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Ashraful Islam on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
